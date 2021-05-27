5bb90800e6e36958c317f91e7c90dcbc72284485
Winner-Python <harmeet3363@gmail.com>
2021-05-27 10:33:02 +0530

    initial commit

---
+++ Chess.py
@@ -0,0 +1,15 @@
+
+import pygame
+pygame.init()
+
+#Create a Window
+#Create a 8x8 Grid
+#Color the Grid
+#Place Sprites in the Grid
+#Assign all Rules of the Game to each Pawn
+
+while 1 == 1:
+    size = height, width = 560, 560
+    screen = pygame.display.set_mode(size)
+pygame.quit()
+
