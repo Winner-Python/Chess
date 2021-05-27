a037932b726081e8ad8ef2118a92dc9cab5e3dc8
shshwtsuthar <shshwtsuthar@gmail.com>
2021-05-27 10:41:19 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init()
+
+#Create a Window
+#Create a 8x8 Grid
+#Color the Grid
+#Place Sprites in the Grid
+#Assign all Rules of the Game to each Pawn
+
+size=height, width=320, 320
+screen=pygame.display.set_mode(size)
+
