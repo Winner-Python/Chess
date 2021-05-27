47502b1aa8a7e17b9bb3845189ccaf877f78271d
shshwtsuthar <shshwtsuthar@gmail.com>
2021-05-27 11:01:08 +0530

    Chess

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import pygame
-pygame.init()
-
 #Create a Window
 #Create a 8x8 Grid
 #Color the Grid
 #Place Sprites in the Grid
 #Assign all Rules of the Game to each Pawn
 
-size=height, width=320, 320
-screen=pygame.display.set_mode(size)
+from tkinter import *
+window=Tk()
+window.title(text="Chess Game")
 
+elephant_white_right=Button(window, text='Elephant', height=10, width=10)
+elephant_white_right.grid(row=0, column=0)
+
+window.mainloop()
